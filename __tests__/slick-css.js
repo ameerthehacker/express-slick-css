55ad2cc755170e08e45ac1ea40ac46738b9f8dc9
ameerthehacker <ameerjhanprof@gmail.com>
2018-08-04 23:05:40 +0530

    Add slickCSS to remove stylesheets and to add slick stylesheets :fire:

--- __tests__/slick-css.js
+++ __tests__/slick-css.js
@@ -1,12 +1,22 @@
-const SlickCSS = require('../src/slick-css');
 let uncss = require('uncss');
 let { error } = require('../src/util');
+let { JSDOM } = require('jsdom');
+const SlickCSS = require('../src/slick-css');
+const path = require('path');
+
+jest.mock('jsdom');
 
 describe('slick-css.js', () => {
   let slickCSS, options, htmlContent;
 
   beforeEach(() => {
-    options = { uncssOptions: {}, uncssFn: uncss, errorFn: error };
+    options = {
+      uncssOptions: {},
+      uncssFn: uncss,
+      errorFn: error,
+      cssPath: 'css',
+      publicPath: '/'
+    };
     slickCSS = new SlickCSS(options);
     htmlContent = `
       <html></html>
@@ -51,6 +61,115 @@ describe('slick-css.js', () => {
 
       return slickCSS.slickify(htmlContent).catch(() => {
         expect(1).toBe(1);
+      });
+    });
+  });
+
+  describe('getDom()', () => {
+    it('should call new JSOM() with htmlContent', () => {
+      slickCSS.getDom(htmlContent);
+
+      expect(JSDOM).toHaveBeenCalledWith(htmlContent);
+    });
+  });
+
+  describe('removeStyleSheets()', () => {
+    let querySelectorMock, removeMock, dom;
+
+    beforeEach(() => {
+      removeMock = jest.fn();
+      querySelectorMock = jest.fn().mockImplementation(() => {
+        return { remove: removeMock };
+      });
+      dom = { window: { document: { querySelector: querySelectorMock } } };
+    });
+
+    it('should call querySelector with correct parameters', () => {
+      const query = 'link[rel="stylesheet"]';
+
+      slickCSS.removeStyleSheets(dom);
+
+      expect(querySelectorMock).toHaveBeenCalledWith(query);
+    });
+
+    it('should call remove on the selected stylesheets', () => {
+      slickCSS.removeStyleSheets(dom);
+
+      expect(removeMock).toBeCalled();
+    });
+  });
+
+  describe('addSlickCSS()', () => {
+    let dom;
+
+    beforeEach(() => {
+      dom = {};
+      slickCSS.saveSlickCSS = jest.fn();
+    });
+
+    it('should reject with error when cssPath is not given', () => {
+      slickCSS.options.errorFn = jest.fn();
+      slickCSS.options.cssPath = undefined;
+      expect.assertions(1);
+
+      return slickCSS.addSlickCSS(dom, htmlContent).catch(() => {
+        expect(slickCSS.options.errorFn).toBeCalled();
+      });
+    });
+
+    it('should reject with error when publicPath is not given', () => {
+      slickCSS.options.errorFn = jest.fn();
+      slickCSS.options.publicPath = undefined;
+      expect.assertions(1);
+
+      return slickCSS.addSlickCSS(dom, htmlContent).catch(() => {
+        expect(slickCSS.options.errorFn).toBeCalled();
+      });
+    });
+
+    it('should reject with error when saveSlickCSS fails', () => {
+      const errMsg = 'some error!';
+      slickCSS.saveSlickCSS = jest.fn().mockRejectedValue(errMsg);
+      expect.assertions(1);
+
+      return slickCSS.addSlickCSS(dom, htmlContent).catch((err) => {
+        expect(err).toBe(errMsg);
+      });
+    });
+
+    it('should attach the sheets properly', () => {
+      const setAttributeMock = jest.fn();
+      const createElementReturnValue = { setAttribute: setAttributeMock };
+      const createElementMock = jest
+        .fn()
+        .mockReturnValue({ setAttribute: setAttributeMock });
+      const appendChildMock = jest.fn();
+      const filePath = `${__dirname}${path.sep}css${path.sep}styles.css`;
+      const fileName = filePath.split(path.sep).pop();
+      slickCSS.saveSlickCSS = jest.fn().mockResolvedValue(filePath);
+      dom = {
+        window: {
+          document: {
+            createElement: createElementMock,
+            head: {
+              appendChild: appendChildMock
+            }
+          }
+        }
+      };
+      expect.assertions(6);
+
+      return slickCSS.addSlickCSS(dom, htmlContent).then(() => {
+        // Check if link element is created properly
+        expect(createElementMock).toBeCalledWith('link');
+        expect(setAttributeMock.mock.calls[0][0]).toBe('href');
+        expect(setAttributeMock.mock.calls[0][1]).toBe(
+          `${options.publicPath}${fileName}`
+        );
+        expect(setAttributeMock.mock.calls[1][0]).toBe('rel');
+        expect(setAttributeMock.mock.calls[1][1]).toBe('stylesheet');
+        // Check if link element is appended properly
+        expect(appendChildMock).toBeCalledWith(createElementReturnValue);
       });
     });
   });
--- src/slick-css.js
+++ src/slick-css.js
@@ -1,4 +1,6 @@
 const Promise = require('bluebird');
+const { JSDOM } = require('jsdom');
+const path = require('path');
 
 class SlickCSS {
   constructor(options) {
@@ -22,6 +24,78 @@ class SlickCSS {
           reject(err);
         }
       });
+    });
+  }
+
+  async saveSlickCSS(htmlContent) {
+    return new Promise((resolve, reject) => {
+      const { fileManager, errorFn } = this.options;
+
+      if (fileManager === undefined) {
+        errorFn('option fileManager not specified');
+        reject();
+
+        return;
+      }
+
+      return this.slickify(htmlContent)
+        .then((usedStyles) => {
+          resolve(fileManager.write(usedStyles));
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  getDom(htmlContent) {
+    const dom = new JSDOM(htmlContent);
+
+    return dom;
+  }
+
+  removeStyleSheets(dom) {
+    const styleSheets = dom.window.document.querySelector(
+      'link[rel="stylesheet"]'
+    );
+
+    styleSheets.remove();
+
+    return dom;
+  }
+
+  async addSlickCSS(dom, htmlContent) {
+    return new Promise((resolve, reject) => {
+      const { cssPath, errorFn, publicPath } = this.options;
+
+      if (cssPath === undefined) {
+        errorFn('option cssPath not specified');
+        reject();
+
+        return;
+      }
+
+      if (publicPath === undefined) {
+        errorFn('option cssPath not specified');
+        reject();
+
+        return;
+      }
+
+      return this.saveSlickCSS(htmlContent)
+        .then((filePath) => {
+          const cssFileName = filePath.split(path.sep).pop();
+          const slickStyleSheet = dom.window.document.createElement('link');
+
+          slickStyleSheet.setAttribute('href', `${publicPath}${cssFileName}`);
+          slickStyleSheet.setAttribute('rel', 'stylesheet');
+          dom.window.document.head.appendChild(slickStyleSheet);
+
+          resolve(dom);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
   }
 }
